47e940ce75308c9ae2f2609ca407d61be76d49df
Luigi Cucciolillo <luigi.cucciolillo@gmail.com>
2025-04-19 23:33:48 +0200

    testing CI 7

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello World! from me</h1>')
+  res.send('<h1>Hello World! after watchtower running.</h1>')
 })
 
 const PORT = 8080
